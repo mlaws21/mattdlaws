1b5da09d6e0ad62b07013773967265760fba7792
Matt <mdl4@williams.edu>
2023-11-13 16:14:29 -0500

    q-refactor

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,35 @@
+import "./style/Contact.css";
+import gmail from './images/gmail.png';
+import linkedin from './images/linkedin.png';
+import github from './images/github.png';
+
+
+
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+
+class Handle extends React.Component {
+
+    render() {
+        return (
+            <div id="com">
+                <img className="logoImg" src={this.props.image} alt="logo" />
+                <p className="cLink">{this.props.site}: {this.props.link}</p>
+            </div>
+        )
+    }
+}
+
 function Contact() {
-    // info about the field / where we play
     return (
         <div>
-            Contact
+            <p id="cTitle">Find me at:</p>
+            <Handle image={gmail} site="Email" link="mdl4@williams.edu"/>
+            <Handle image={linkedin} site="Linkedin" link="linkedin.com/in/matt-d-laws/"/>
+            <Handle image={github} site="Github" link="github.com/mlaws21"/>
+
+
         </div>
     )
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,96 @@
+import React from 'react';
+
+import "./style/Projects.css";
+import data from '../Data/projects.jsx';
+import i1 from './images/wrfc.png';
+import i2 from './images/minigolf.png';
+import bi1 from './images/wrfcPage.png';
+import bi2 from './images/minigolf.png';
+const images = [i1, i2]
+const bigImages = [bi1, bi2]
+
+
+
+class ProjContainer extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            status: true
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
+    
+    handleClick() {
+        this.setState({status: !this.state.status});
+    }
+
+
+    render() {
+        if (this.state.status) {
+            return (
+                <div className="projClose" onClick={this.handleClick}>
+                    <p className="projTitle">{this.props.title}</p>
+                    <img className="img" src={this.props.image} />
+
+                </div>
+                
+            )
+        } else {
+            return (
+                <div>
+                    <div id="else" onClick={this.handleClick}></div>
+                    <div className="projClose">
+                        <p className="projTitle">{this.props.title}</p>
+                        <img className="img" src={this.props.image} />
+
+
+                    </div>
+                    
+                    <div className="projOpen">
+                        {/* <p id="close">x</p> */}
+                        <p className="bigTitle">{this.props.exTitle}</p>
+                        <p id="description">{this.props.description}</p>
+                        <div id="demo">
+                            <img id="bigImage" src={this.props.bigImage} /> 
+                            <a id="link"href={this.props.link} target="_blank">{this.props.linkDescription}</a>
+
+                        </div>
+                        
+
+
+
+                    </div>
+                    
+                </div>
+                
+            )
+        }
+
+    }
+
+}
+
+function build() {
+    var all = [];
+    for (var i = 0; i < data.length; i++) {
+        all.push(<ProjContainer 
+            title={data[i][0]} 
+            exTitle={data[i][1]} 
+            image={images[i]} 
+            bigImage={bigImages[i]} 
+            description={data[i][2]} 
+            link={data[i][3]}
+            linkDescription={data[i][4]}
+             />)
+    }
+    return (all);
+}
+
 function Projects() {
-    // info about the field / where we play
+
     return (
-        <div>
-            Projects
+        <div id="projects">
+            {build()}
         </div>
     )
 }
